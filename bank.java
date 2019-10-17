42943df113d7b29bf465ae3ce96eaee23babb1a5
Dee1810 <46949560+Dee1810@users.noreply.github.com>
2019-10-17 19:28:46 +0530

    Add files via upload

--- bank.java
+++ bank.java
@@ -1,260 +1,150 @@
-import java.util.Scanner;
-
-class BankWork
+import java.util.*;
+class account
 {
-          final int max_limit=5;
-          final  int min_limit=1;
-          final double min_bal=500;
-         private  String name[]=new String[5];
-         private int accNo[]=new int[5];
-         private  String accType[]=new String[5];
-         private double balAmt[]=new double[5];
-         static int totRec=0;
-        //constructor
-        BankWork()
-        {
-             for(int i=0;i<max_limit;i++)
-             {
-                name[i]="";
-                accNo[i]=0;
-                accType[i]="";
-                balAmt[i]=0.0;
-            }
-        }
-
-        
-        
-        //TO  ADD NEW RECORD
-public void newEntry()
-        {
-              /* String str;
-               int acno;
-               double amt;*/
-               
-               
-               
-               totRec++;         // Incrementing Total Record               
-               System.out.println("\n\n\n=====RECORDING NEW ENTRY=====");
-               
-                          accNo[totRec]=totRec;    //Created  AutoNumber  to accNo so no invalid id occurs
-                        System.out.println("Account Number :  "+accNo[totRec]);
-                        
-               Scanner s=new Scanner(System.in);
-                     System.out.print("Enter Name :  ");
-                   //  System.out.flush();
-                     name[totRec]=s.nextLine();
-
-                     System.out.print("Enter Account Type : ");
-                     //System.out.flush();
-                     accType[totRec]=s.nextLine();
-
-                    
-                           System.out.print("Enter Initial  Amount to be deposited : "); balAmt[totRec]=s.nextDouble();
-                       //    System.out.flush();
-                         /*  str=s.nextLine();
-                           balAmt[totRec]=Double.parseDouble(str);
-                               //Validation that minimun amount must be 500*/
-
-                
-            
-        }
-
-        
-        
-        //TO DISPLAY DETAILS OF RECORD
-public void display()
-        {
-            
-            
-              //boolean valid=true;
-                           
-              System.out.println("\n\n=====DISPLAYING DETAILS OF CUSTOMER=====\n");
-              
-              //   BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
-/*Scanner obj=new Scanner(System.in);                
- System.out.print("Enter Account number : ");
-                 System.out.flush();
-                 str=obj.nextLine();
-                // acno=Integer.parseInt(str);*/
-                  
-                 
-                        System.out.println("\n\nAccount Number : "+accNo[acno]);
-                        System.out.println("Name : "+name[acno]);
-                        System.out.println("Account Type : "+accType[acno]);
-                        System.out.println("Balance Amount : "+balAmt[acno]+"\n\n\n");
-                      
-                 
-        }
-
-
-
-          //TO DEPOSIT AN AMOUNT
-public void deposit()
-        {
-             // String str;
-              double amt;
-            /*  int acno;
-              //boolean valid=true;
-              System.out.println("\n\n\n=====DEPOSIT AMOUNT=====");
-              
-              //try{
-                   //Reading deposit value
-                //   BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
-              Scanner obj=new Scanner(System.in);
-                        System.out.print("Enter Account No : ");
-                        System.out.flush();
-                        str=obj.nextLine();
-                        acno=Integer.parseInt(str);
-                         if (acno<min_limit || acno>totRec)   //To check whether accNo is valid or Not
-                         {
-                              System.out.println("\n\n\nInvalid Account Number \n\n");
-                              //valid=false;
-                         }
-           
-                        else
-                       {*/
-                            System.out.print("Enter Amount you want to Deposit  : ");
-                            System.out.flush();
-                            str=obj.nextLine();
-                            amt=Double.parseDouble(str);
-
-                            balAmt[acno]=balAmt[acno]+amt;
-
-                            //Displaying Depsit Details
-                            System.out.println("\nAfter Updation...");
-                            System.out.println("Account Number :  "+acno);
-                            System.out.println("Balance Amount :  "+balAmt[acno]+"\n\n\n");
-                        
-                
-       }
-
-
-
-     //TO WITHDRAW BALANCE
-public void withdraw()
-        {
-            //  String str;
-              double amt,checkamt;
-              //int acno;
-              //boolean valid=true;
-              System.out.println("\n\n\n=====WITHDRAW AMOUNT=====");
-              
-              //try{
-                   //Reading deposit value
-                //   BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
-                /*  Scanner obj=new Scanner(System.in); 
-                        System.out.print("Enter Account No : ");
-                        System.out.flush();
-                        str=obj.nextLine();
-                        acno=Integer.parseInt(str);
-
-                          if (acno<min_limit || acno>totRec)   //To check whether accNo is valid or Not
-                             {
-                                System.out.println("\n\n\nInvalid Account Number \n\n");
-                                //valid=false;
-                            }
-
-                        else
-                        {*/
-                                System.out.println("Balance is : "+balAmt[acno]);
-                                System.out.print("Enter Amount you want to withdraw  : ");
-                                System.out.flush();
-                                str=obj.nextLine();
-                                amt=Double.parseDouble(str);
-
-                                checkamt=balAmt[acno]-amt;
-
-                                if(checkamt >= min_bal)
-                                 {
-                                    balAmt[acno]=checkamt;
-                                    //Displaying Depsit Details
-                                    System.out.println("\nAfter Updation...");
-                                    System.out.println("Account Number :  "+acno);
-                                    System.out.println("Balance Amount :  "+balAmt[acno]+"\n\n\n");
-                                }
-                                else
-                                 {
-                                    System.out.println("\n\nAs per Bank Rule you should maintain minimum balance of Rs 500\n\n\n");
-                                }
-                        
-       
-
-}
-
-public class bank
-{
-    public static void main(String args[]) 
+	String name;
+	String accno;
+	String type;
+	double bal;
+	final double min=500;
+	Scanner s=new Scanner(System.in);
+    account()
     {
-        int acno;
-        int choice;
-        choice=0;
-      for(int i=0;i<5;i++)
-        BankWork BW_obj[i] = new BankWork();
-        
+    	name="";
+    	accno="";
+    	type="";
+    	bal=0;
+    }
+    void read()
+    {
+    	System.out.println("Enter acc no:");
+    	accno=s.nextLine();
+    	System.out.println("Enter name:");
+    	name=s.next();
+    	System.out.println("Enter acc type:");
+    	type=s.next();
+    	System.out.println("Enter acc balance:");
+    	bal=s.nextDouble();
+      
 
-        for(;;)
-        {System.out.println("Choose Your Choices ...");
-        System.out.println("1) New Record Entry ");
-        System.out.println("2) Display Record Details ");
-        System.out.println("3) Deposit...");
-        System.out.println("4) Withdraw...");
-        System.out.println("5) Exit");
-         System.out.print("Enter your choice :  ");
-        System.out.flush();
-              //try{
-                //   BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
-Scanner obj=new Scanner(System.in);                  
- str=obj.nextLine();
-                   choice=Integer.parseInt(str);
+    }
+    void display()
+    {
+    	//String a=s.nextLine();
+    	System.out.println("Enter acc no:"+accno);
+    	//accno=s.nextLine();
+    	System.out.println("Enter name:"+name);
+    	//name=s.next();
+    	System.out.println("Enter acc type:"+type);
+    	//type=s.next();
+    	System.out.println("Enter acc balance:"+bal);
+    	//bal=s.nextDouble();
+    		/*if(a.equals(accno))
+    		{
+    			System.out.println("Name: "+name+"Account num: "+accno+"Account type: "+type+"Balance: "+bal);
+    			break;
+    		}*/
+    	
+    }
+    void deposit()
+    {
+    	System.out.println("Enter the amount to be deposited");
+    	double amt=s.nextDouble();
+    	bal=bal+amt;
+    	display();
+    }
+    void withdraw()
+    {
+    	System.out.println("Available balance:"+bal+"\nEnter the amount to be withdrawn:");
+    	double amt=s.nextDouble();
+    	if((bal-amt)>=min)
+    	{bal=bal-amt;
+    	System.out.println("Enter acc no:"+accno);
+    	//accno=s.nextLine();
+    	System.out.println("Enter name:"+name);
+    	//name=s.next();
+    	System.out.println("Enter acc type:"+type);
+    	//type=s.next();
+    	System.out.println("Enter acc balance:"+bal);	
+    	}
+    	else
+    	{
+    		System.out.println("Insuffiecient balanace");
 
-                          switch(choice)
-                           {
-                            case 1 :  //New Record Entry
-                                                if (totRec<min_limit  || totRec>5)   //To check whether accNo is valid or Not
-                     {
-                          System.out.println("\n\n\nInvalid Account Number \n\n");
-                                               }
-                                              else
-                                            BW_obj[totRec].newEntry();
-                                           break;
-                            case 2 :  //Displaying Record Details
-                                              Scanner obj=new Scanner(System.in);                
- System.out.println("Enter Account number : ");
-                 acno=obj.nextInt();
-                                                if (acno<min_limit  || acno>totRec)   //To check whether accNo is valid or Not
-                     {
-                          System.out.println("\n\n\nInvalid Account Number \n\n");
-                                               }
-                                              else
-                                           BW_obj[acno-1].display();
-                                           break;
-                            case 3 : //Deposit...
-                                               Scanner obj=new Scanner(System.in);                
- System.out.println("Enter Account number : ");
-                 acno=obj.nextInt();
-                                                if (acno<min_limit  || acno>totRec)   //To check whether accNo is valid or Not
-                     {
-                          System.out.println("\n\n\nInvalid Account Number \n\n");
-                                               }
-                                              else 
-                                            BW_obj[acno-1].deposit();
-                                           break;
-                            case 4 : //Withdraw...
-                                                Scanner obj=new Scanner(System.in);                
- System.out.println("Enter Account number : ");
-                 acno=obj.nextInt();
-                                                if (acno<min_limit  || acno>totRec)   //To check whether accNo is valid or Not
-                     {
-                          System.out.println("\n\n\nInvalid Account Number \n\n");
-                                               }
-                                              else
-                                            BW_obj[acno-1].withdraw();
-                                            break;
-                            case 5  :  System.out.println("\n\n.....THANKS FOR VISITING.....");
-                                            break;
-                            default : System.out.println("\nInvalid Choice \n\n");
-                          }
-                    
+    	}
+    }
 }
+class bank2
+{
+	account a[]=new account[5];
+	Scanner s=new Scanner(System.in);
+  int flag=0;
+	for(int i=0;i<5;i++)
+	{ a[i]=new account();
+		a[i].read();
+
+	}
+	int ch=s.nextInt();
+	switch(ch)
+	{
+		case 1:System.out.println("Enter the accno");
+		       String acc=s.nextLine();
+		       flag=0;
+		       for(int i=0;i<5;i++)
+		       {
+		       	if(acc.equals(a[i].accno))
+		       	{
+		       		a[i].display();
+		       		flag=1;
+		       		break;
+		       	}
+
+		       }
+		       else
+		       {
+		       	printf("Invalid accno");
+
+		       }break;
+		 case 2:System.out.println("Enter the accno");
+		       String acc=s.nextLine();
+		       flag=0;
+		       for(int i=0;i<5;i++)
+		       {
+		       	if(acc.equals(a[i].accno))
+		       	{
+		       		a[i].deposit();
+		       		flag=1;
+		       		break;
+		       	}
+
+		       }
+		       else
+		       {
+		       	printf("Invalid accno");
+		       	
+		       }
+		       break;
+		  case 3:     System.out.println("Enter the accno");
+		       String acc=s.nextLine();
+		       flag=0;
+		       for(int i=0;i<5;i++)
+		       {
+		       	if(acc.equals(a[i].accno))
+		       	{
+		       		a[i].withdraw();
+		       		flag=1;
+		       		break;
+		       	}
+
+		       }
+		       else
+		       {
+		       	printf("Invalid accno");
+		       
+		       }break;
+		 default:  System.exit(0);    
+	}
 }
-}
+
+
+    
+
